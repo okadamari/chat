bda42a9d65010d67f7305f7562f448a5f5b9a178
okada <47173004+okadamari@users.noreply.github.com>
2021-03-14 08:37:00 +0900

    move firebaseConfig to main.js

--- src/App.vue
+++ src/App.vue
@@ -29,20 +29,6 @@ export default {
     };
   },
   created: function() {
-    var firebaseConfig = {
-      apiKey: process.env.VUE_APP_API_KEY,
-      authDomain: process.env.VUE_APP_AUTH_DOMAIN,
-      projectId: process.env.VUE_APP_PROJECT_ID,
-      storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
-      messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
-      appId: process.env.VUE_APP_APP_ID,
-      measurementId: process.env.VUE_APP_MEASUREMENT_ID,
-    };
-
-    console.log(firebaseConfig);
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
-
     const db = firebase.firestore()
     messageRef = db.collection("chat_messages")
 
