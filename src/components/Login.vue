2120ee840022dd05785284942142a6177c396699
okada <47173004+okadamari@users.noreply.github.com>
2021-03-14 12:20:51 +0900

    add login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,11 +1,44 @@
 <template>
-  ログイン
+  <div class="signin">
+    <table>
+        <tr>
+          <th>メールアドレス：</th>
+        </tr>
+        <tr>
+          <td><input type="email" v-model="mailaddress"/></td>
+        </tr>
+        <tr>
+          <th>パスワード：</th>
+        </tr>
+        <tr>
+          <td><input type="password" v-model="password"/></td>
+        </tr>
+      </table>
+      <button @click="login">ログイン</button>
+  </div>
 </template>
 
 <script>
+import firebase from 'firebase'
 
 export default {
   name: 'login',
+  methods: {
+    login: function () {
+      firebase.auth().signInWithEmailAndPassword(this.mailaddress, this.password)
+        .then(() => {
+          // Signed in
+          this.$router.replace(this.$route.query.redirect || '/chat')
+        })
+        .catch((error) => {
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          alert('ログインに失敗しました')
+          console.log( errorCode)
+          console.log( errorMessage)
+        });
+    }
+  }
 }
 </script>
 
